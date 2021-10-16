b7b55cf0ae5503ef8008f7695d7cb0f72d82d3d1
Erik Lundin <erilu186@student.liu.se>
2021-10-16 15:35:10 +0200

    rudimentary python script structure

--- src/drl/train.py
+++ src/drl/train.py
@@ -2,3 +2,21 @@
 # Retrieve state dict of latest, best model
 # Train using that model and the self play dataset
 # Evaluate new model, if better, save state dict
+
+def load_data():
+    return
+
+def load_model():
+    return
+
+def train_model():
+    return
+
+def evaluate_and_set_weights():
+    return
+
+def main():
+    dataset = load_data()
+    model = load_model()
+    train_model(model)
+    evaluate_and_set_weights(model)
