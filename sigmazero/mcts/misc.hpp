2ead0884ae3b0969f461155dee1b65a3017e5afd
Erik Lundin <c.erik.lundin@gmail.com>
2021-11-08 16:29:18 +0100

    make misc.cpp function non-inline

--- sigmazero/mcts/misc.hpp
+++ sigmazero/mcts/misc.hpp
@@ -9,45 +9,26 @@
 
 // Retrieve an iterator at a random position
 template<typename Iterator, typename RandomGenerator>
-inline Iterator random_element(Iterator start, Iterator end, RandomGenerator& generator)
-{
-    std::uniform_int_distribution<> dist(0, std::distance(start, end)-1);
-    std::advance(start, dist(generator));
-    return start;
-};
+Iterator random_element(Iterator start, Iterator end, RandomGenerator& generator);
 
 // Retrieve a random element between two iterators
 template<typename Iterator>
-inline Iterator random_element(Iterator start, Iterator end)
-{
-    static std::random_device random_device;
-    static std::mt19937 generator(random_device());
-    return random_element(start, end, generator);
-};
+Iterator random_element(Iterator start, Iterator end);
 
 // Get the index of the maximum element between two iterators
 template<typename Iterator>
-inline size_t get_max_idx(Iterator start, Iterator end)
-{
-    return std::distance(start, std::max_element(start, end));
-};
+size_t get_max_idx(Iterator start, Iterator end);
 
 // Get the max element between two iterators
 template<typename T, typename Iterator1, typename Iterator2>
-inline T get_max_element(Iterator1 wanted_start, Iterator2 comparator_start, Iterator2 comparator_end)
-{
-    size_t max_idx = get_max_idx(comparator_start, comparator_end);
-    std::advance(wanted_start, max_idx);
-    return *wanted_start;
-};
+T get_max_element(Iterator1 wanted_start, Iterator2 comparator_start, Iterator2 comparator_end);
+
 // Simple timer class for measuring execution time
 struct Timer
 {
     Timer();
     void set_start();
-    
     double get_time(bool setstart=false);
-
     std::chrono::time_point<std::chrono::high_resolution_clock> start_t = std::chrono::high_resolution_clock::now();
 };
 
