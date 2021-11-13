6d075456add595d05aaef47c9e652f6c9227b07b
Erik Lundin <c.erik.lundin@gmail.com>
2021-11-13 21:45:13 +0100

    update test_drive to perform 1 mcts iteration

--- sigmazero/test_drive.cpp
+++ sigmazero/test_drive.cpp
@@ -1,6 +1,8 @@
 #include <sigmazero/drl/sigmanet.hpp>
+#include <sigmazero/mcts/node.hpp>
 #include <chess/chess.hpp>
 #include <iostream>
+#include <memory>
 /*
 Perform mcts with an untrained sigmanet to verify that the program does not crash
 */
@@ -20,5 +22,24 @@ int main()
     }    
 
     std::cout << "position value: " << evaluation.first << std::endl;
+
+    std::cout << "attempting one monte carlo tree search..." << std::endl;
+
+
+    std::shared_ptr<mcts::Node> main_node{std::make_shared<mcts::Node>(start)};
+    std::pair<double, std::unordered_map<size_t, double>> evaluation = network.evaluate(start);
+    main_node->explore_and_set_priors(evaluation);
+    main_node->add_exploration_noise(0.3, 0.25);
+    for(int i = 0 ; i < 100 ; ++i)
+    {
+        std::shared_ptr<mcts::Node> current_node = main_node->traverse();
+        if(!current_node->is_over()) {
+            current_node->backpropagate(current_node->get_terminal_value());
+            continue;
+        }
+        evaluation = network.evaluate(current_node->get_state());
+        current_node->explore_and_set_priors(evaluation);
+    }
+
     return 0;
 }
